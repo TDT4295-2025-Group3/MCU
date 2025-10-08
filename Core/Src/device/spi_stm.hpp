95cb1d827de61eac2868855d4c6aabe5652e234b
Mikke B. Eggen <mikkeleg@stud.ntnu.no>
2025-10-08 13:01:45 +0200

    Wrong file name xdddd

--- Core/Src/device/spi_stm.hpp
+++ Core/Src/device/spi_stm.hpp
@@ -1,6 +1,8 @@
-// fixed_point.hpp
-#pragma once
-#include <cstdint>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // Define the raw type for Q16.16
 using Q16_16 = int32_t;
@@ -15,3 +17,11 @@ inline Q16_16 floatToQ16_16(float value) {
 inline float q16_16ToFloat(Q16_16 value) {
     return static_cast<float>(value) / (1 << 16);
 }
+uint8_t wipe_all(void);
+uint16_t create_vertex(uint32_t *vertexBuffer, uint32_t vertCount);
+uint16_t create_triangle(uint32_t *triangleBuffer, uint32_t triCount);
+uint16_t create_instance(uint32_t *instanceBuffer, uint32_t instCount);
+uint8_t update_instance(uint32_t *instanceBuffer, uint32_t instCount);
+#ifdef __cplusplus
+}
+#endif
